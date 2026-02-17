ebb82d3e2305523d514f7fe8f2964d246d223b87
Kim Cyriel S. Avillanosa <careers.kmavillanosa@gmail.com>
2026-02-17 11:16:23 +0800

    update styles

--- api/src/modules/auth/signin-dto.ts
+++ api/src/modules/auth/signin-dto.ts
@@ -1,8 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class SignInDto {
   @ApiProperty({ default: '', required: false })
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
   @ApiProperty({ default: '', required: false })
+  @IsString()
+  @IsNotEmpty()
   password: string;
 }
--- api/src/modules/users/dto/create-user.dto.ts
+++ api/src/modules/users/dto/create-user.dto.ts
@@ -1,26 +1,44 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 
 export class CreateUserDto {
   @ApiProperty({ default: "", required: true })
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
   @ApiProperty({ default: "", required: true })
+  @IsString()
+  @IsNotEmpty()
   password: string;
 
   @ApiProperty({ default: "", required: true })
+  @IsString()
+  @IsNotEmpty()
   name: string;
 
   @ApiProperty({ default: "", required: true })
+  @IsString()
+  @IsNotEmpty()
   address: string;
 
   @ApiProperty({ default: "", required: true })
+  @IsString()
+  @IsNotEmpty()
   bank_name: string;
 
   @ApiProperty({ default: "", required: true })
+  @IsString()
+  @IsNotEmpty()
   bank_swift_code: string;
 
   @ApiProperty({ default: "", required: true })
+  @IsString()
+  @IsNotEmpty()
   bank_account_number: string;
 
   @ApiProperty({ default: "", required: true })
+  @IsString()
+  @IsNotEmpty()
   bank_account_name: string;
 }
